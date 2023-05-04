a337abd6e743d1c36cdecd25443347c5a118e8b0
unaabcxyz <tkimhyung1230@gmail.com>
2023-05-04 10:43:07 +0700

    tab layout

---
+++ app/src/main/java/com/dwiauliaanugerah_202102359/tablayout/MyFragmentPagerAdapter.java
@@ -0,0 +1,49 @@
+package com.dwiauliaanugerah_202102359.tablayout;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.lang.ref.PhantomReference;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter
+{
+    private BeritaFragment _beritaFragment = new BeritaFragment();
+    private Context _context;
+    private ECommerceFragment _eCommerceFragment = new ECommerceFragment();
+    private int _tabCount;
+    private KampusFragment _kampusFragment = new KampusFragment();
+
+    public MyFragmentPagerAdapter(Context context, FragmentManager fragmentManager, int tabCount)
+    {
+        super(fragmentManager);
+
+        _context = context;
+        _tabCount = tabCount;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return _eCommerceFragment;
+            case 1:
+                return _beritaFragment;
+            default:
+                return _kampusFragment;
+        }
+    }
+
+    @Override
+    public int getCount()
+    {
+        return _tabCount;
+    }
+
+}
